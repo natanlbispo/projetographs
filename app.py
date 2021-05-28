0011c3db22f545ea571dc8a6759e54ba74ee50fc
Natan L. Bispo <natan.bispo9@gmail.com>
2021-05-28 01:16:45 -0300

    GET LOUD

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ from io import BytesIO
 import os
 import networkx as nx
 from networkx.algorithms.flow import dinitz
+from dinic import MaxFlow
+C = [[ 0, 16, 13, 0, 0, 0 ],  # s
+     [ 0, 0, 10, 12, 0, 0 ],  # o
+     [ 0, 4, 0, 0, 14, 0 ],  # p
+     [ 0, 0, 9, 0, 0, 20 ],  # q
+     [ 0, 0, 0, 7, 0, 4],  # r
+     [ 0, 0, 0, 0, 0, 3 ]]  # t
+
 app=Flask(__name__)
 
-# @app.route('/', methods=['POST', 'GET'])
 @app.route('/',  methods=['POST', 'GET'])
 def index():
     figure = plt.figure()
@@ -15,27 +22,24 @@ def index():
     print()
     path = os.path.abspath(__file__).strip('app.py')
     save = path + '/static/Graph.png'
-    save2 = path + '/static/Graph2.png'
     try:
         if(os.path.exists(save)):
             os.remove(save)
-        if(os.path.exists(save2)):
-            os.remove(save2)
     except:
         pass
 
 
     G = nx.DiGraph()
 
-    G.add_edge("x", "a", capacity=16 );
-    G.add_edge("x", "b", capacity=13 );
-    G.add_edge("a", "b", capacity=10 );
-    G.add_edge("a", "c", capacity=12 );
-    G.add_edge("b", "a", capacity=4 );
+    G.add_edge("x", "a", capacity=16);
+    G.add_edge("x", "b", capacity=13);
+    G.add_edge("a", "b", capacity=10);
+    G.add_edge("a", "c", capacity=12);
+    G.add_edge("b", "a", capacity=4);
     G.add_edge("b", "d", capacity=14);
-    G.add_edge("c", "b", capacity=9 );
-    G.add_edge("c", "e", capacity=20 );
-    G.add_edge("d", "c", capacity=7 );
+    G.add_edge("c", "b", capacity=9);
+    G.add_edge("c", "e", capacity=20);
+    G.add_edge("d", "c", capacity=7);
     G.add_edge("d", "e", capacity=4);
     pos=nx.spring_layout(G) 
     nx.draw(G,pos, with_labels = True,)
@@ -46,12 +50,12 @@ def index():
     if request.method == 'POST':
         data = request.form.to_dict()
         try:
-            R = dinitz(G, data['content0'], data['content2'])          
-            flow_value = nx.maximum_flow_value(G,data['content0'], data['content2'])
-            print (str(flow_value))
+            test = MaxFlow(C,int(data['content0']),int(data['content2']))
+            print (test)
         except nx.NetworkXError:
-            pass
-        return render_template('resultado.html', flow = str(flow_value))
+            pass       
+        
+        return render_template('resultado.html', flow = str(test), entrada="x", sainda="y")
 
     else:
         return render_template('index.html')
